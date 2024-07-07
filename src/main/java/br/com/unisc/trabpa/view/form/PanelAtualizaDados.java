6707f6004f43bd96704ae2579e7a02ba0b1164a6
caiokirst <caiokirst@gmail.com>
2024-07-07 20:25:59 -0300

    Atualizado 20:25

--- src/main/java/br/com/unisc/trabpa/view/form/PanelAtualizaDados.java
+++ src/main/java/br/com/unisc/trabpa/view/form/PanelAtualizaDados.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.Iterator;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 import javax.swing.JOptionPane;
 import javax.swing.SwingWorker;
 
@@ -49,39 +50,39 @@ public class PanelAtualizaDados extends javax.swing.JPanel {
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
         this.setLayout(layout);
         layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(67, 67, 67)
-                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                            .addComponent(dateChooserFim, javax.swing.GroupLayout.DEFAULT_SIZE, 259, Short.MAX_VALUE)
-                            .addComponent(dateChooserInicio)
-                            .addComponent(lblStatus, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
-                    .addGroup(layout.createSequentialGroup()
-                        .addGap(338, 338, 338)
-                        .addComponent(btnAtualizar)))
-                .addContainerGap(351, Short.MAX_VALUE))
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(layout.createSequentialGroup()
+                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                        .addGroup(layout.createSequentialGroup()
+                                                .addGap(67, 67, 67)
+                                                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                                        .addComponent(dateChooserFim, javax.swing.GroupLayout.DEFAULT_SIZE, 259, Short.MAX_VALUE)
+                                                        .addComponent(dateChooserInicio)
+                                                        .addComponent(lblStatus, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                                        .addGroup(layout.createSequentialGroup()
+                                                .addGap(338, 338, 338)
+                                                .addComponent(btnAtualizar)))
+                                .addContainerGap(351, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addGap(68, 68, 68)
-                .addComponent(dateChooserInicio, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(37, 37, 37)
-                .addComponent(dateChooserFim, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addGap(39, 39, 39)
-                .addComponent(lblStatus)
-                .addGap(113, 113, 113)
-                .addComponent(btnAtualizar)
-                .addContainerGap(162, Short.MAX_VALUE))
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGroup(layout.createSequentialGroup()
+                                .addGap(68, 68, 68)
+                                .addComponent(dateChooserInicio, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(37, 37, 37)
+                                .addComponent(dateChooserFim, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                .addGap(39, 39, 39)
+                                .addComponent(lblStatus)
+                                .addGap(113, 113, 113)
+                                .addComponent(btnAtualizar)
+                                .addContainerGap(162, Short.MAX_VALUE))
         );
     }
 
-    private void btnAtualizarActionPerformed(java.awt.event.ActionEvent evt) {                                             
+    private void btnAtualizarActionPerformed(java.awt.event.ActionEvent evt) {
         Date dataInicio = dateChooserInicio.getDate();
         Date dataFim = dateChooserFim.getDate();
-
+       
         if (dataInicio == null || dataFim == null) {
             JOptionPane.showMessageDialog(this, "Por favor, selecione ambas as datas.", "Erro de Validação", JOptionPane.ERROR_MESSAGE);
             return;
@@ -89,6 +90,15 @@ public class PanelAtualizaDados extends javax.swing.JPanel {
 
         if (!dataFim.after(dataInicio)) {
             JOptionPane.showMessageDialog(this, "A data final deve ser maior que a data inicial.", "Erro de Validação", JOptionPane.ERROR_MESSAGE);
+            return;
+        }
+        
+        // Calcula a diferença em dias
+        long difEmMiliSec = Math.abs(dataFim.getTime() - dataInicio.getTime());
+        long difEmDias = TimeUnit.DAYS.convert(difEmMiliSec, TimeUnit.MILLISECONDS);
+        
+        if (difEmDias > 7){
+            JOptionPane.showMessageDialog(this, "O intervalo deve ser de no máximo 1 semana.", "Erro de Validação", JOptionPane.ERROR_MESSAGE);
             return;
         }
 
@@ -119,5 +129,5 @@ public class PanelAtualizaDados extends javax.swing.JPanel {
                 btnAtualizar.setEnabled(true);
             }
         }.execute();
-    }                                            
+    }
 }
